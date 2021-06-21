9a104aaa61db34f1f9b5d21454852cb0607c9be1
ahmedshahrour <ahmed89sh@gmail.com>
2021-06-21 15:16:49 +0300

    lab11-busmall

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ let imgArray = [
   'boots.jpg',
   'breakfast.jpg',
   'bubblegum.jpg',
-  'chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png',
-  'tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg','wireframe.png'
+  'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png',
+  'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg', 'wireframe.png'
 ];
 
 // Constructor
@@ -19,37 +19,46 @@ let imgArray = [
 // Event Handler
 
 let imageSection = document.getElementById('imageSection');
-let leftImage = document.getElementById( 'leftImage' );
-let rightImage = document.getElementById( 'rightImage' );
-let centerImage = document.getElementById( 'centerImage' );
-let listCont = document.getElementById('listCont');
-let counter = 0;
+let leftImage = document.getElementById('leftImage');
+let rightImage = document.getElementById('rightImage');
+let centerImage = document.getElementById('centerImage');
+let proceed = document.getElementById('proceed');
+let report = document.getElementById('report');
 
-function Images( name, src ) {
+// let listCont = document.getElementById('listCont');
+
+let counter = 0;
+// let round = 25;
+
+let leftIndex;
+let rightIndex;
+let centerIndex;
+
+function Images(name, src) {
   this.name = name;
   this.src = `./img/${src}`;
   this.views = 0;
+  this.click = 0;
   Images.all.push(this);
 }
 
 Images.all = [];
 
-for( let i = 0; i < imgArray.length; i++ ) {
+for (let i = 0; i < imgArray.length; i++) {
   // console.log(imgArray[i].split( '.' ));
-  new Images( imgArray[i].split( '.' )[0], imgArray[i] );
+  let imagename = imgArray[i].split('.')[0];
+  new Images(imagename, imgArray[i]);
 }
 
 function render() {
-  let leftIndex = randomNumber(0, imgArray.length - 1);
-  let rightIndex;
-  let centerIndex;
+  leftIndex = randomNumber(0, imgArray.length - 1);
 
   do {
     rightIndex = randomNumber(0, imgArray.length - 1);
     centerIndex = randomNumber(0, imgArray.length - 1);
-  } while( leftIndex === rightIndex ||centerIndex === leftIndex || rightIndex === centerIndex);
+  } while (leftIndex === rightIndex || centerIndex === leftIndex || rightIndex === centerIndex);
 
-  
+
 
   rightImage.src = Images.all[rightIndex].src;
   leftImage.src = Images.all[leftIndex].src;
@@ -64,16 +73,38 @@ function render() {
 
 function eventHandler(e) {
   // console.log(e.target.id);
-  if((e.target.id === 'rightImage' || e.target.id === 'leftImage' || e.target.id === 'centerImage') && counter < 25){
-    render();
-    console.log(counter);
-    counter++;
+  if ((e.target.id === 'rightImage' || e.target.id === 'leftImage' || e.target.id === 'centerImage') && counter < 25) {
 
+    console.log(counter);
+
+    if (e.target.id === 'rightImage') {
+      Images.all[rightIndex].click++;
+    }
+    if (e.target.id === 'centerImage') {
+      Images.all[centerIndex].click++;
+    }
+    if (e.target.id === 'leftImage') {
+      Images.all[leftIndex].click++;
+    }
+    console.log(Images.all);
+    render();
+    counter++;
   }
 
 }
 
+function printresult(event){
+for (let i= 0 ; i < Images.all.length ; i++){
+  let li = document.createElement('li');
+  report.appendChild(li);
+  li.textContent = `${Images.all[i].name} had ${Images.all[i].click} votes, and was seen ${Images.all[i].views} times.`
+}
+
+proceed.removeEventListener('click' , printresult);
+}
+
 imageSection.addEventListener('click', eventHandler);
+proceed.addEventListener('click', printresult);
 
 render();
 
@@ -84,8 +115,8 @@ render();
 // console.log( leftImage, rightImage );
 
 // Helper function
-function randomNumber( min, max ) {
-  min = Math.ceil( min );
-  max = Math.floor( max );
-  return Math.floor( Math.random() * ( max - min + 1 ) + min ); //The maximum is inclusive and the minimum is inclusive
+function randomNumber(min, max) {
+  // min = Math.ceil( min );
+  // max = Math.floor( max );
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
